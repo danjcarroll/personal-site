359081fb73e46515ebb5c8e05e0eced21114c4fd
primalrambler <primal.rambler@gmail.com>
2016-08-19 11:03:07 -0500

    linked to bootstrap CSS CDN

--- public/index.php
+++ public/index.php
@@ -11,8 +11,8 @@
 
     <title>WELCOME</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Custom CSS -->
     <link href="/css/landing-page.css" rel="stylesheet">
