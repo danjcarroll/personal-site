47f543d0c7826236086e0a896550347b3c66d3a7
primalrambler <primal.rambler@gmail.com>
2016-08-24 16:49:11 -0500

    created portfolio page from resume page 'template'. added 6 content areas for portfolio showcasing.

---
+++ public/portfolio/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="resume web developer javascript php html css">
+    <meta name="author" content="Daniel Carroll">
+
+    <title>DjC | Portfolio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link href="/css/index.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static-top topnav navbar-inverse" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav brand-text" href="/index.php">DjC</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="/portfolio/">PORTFOLIO</a>
+                    </li>
+                    <li>
+                        <a href="/resume/">RESUME</a>
+                    </li>
+                    <li class="btn-contact">
+                        <a href="contact.php">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+
+    <!-- Intro -->
+    <a id="main"></a>
+    <div class="section-main img-resume-bg">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h3>what I've been working on recently</h3>
+                        <h1>PORT<span class="lg-text">f</span>OLIO</h1>
+                        <hr class="intro-divider">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro -->
+	
+	<div class="content-section-b">
+        <div class="container">
+        	<a id="quote"></a>
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                	<h2 class="section-heading">Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-a -->   
+
+    <div class="content-section-a">
+        <div class="container">
+        	<a id="portfolio"></a>
+            <div class="row">
+                <div class="col-xs-12 col-sm-4"><div class="box-portfolio">Weather Map</div></div>
+                <div class="col-xs-12 col-sm-4"><div class="box-portfolio">Simple Simon</div></div>
+                <div class="col-xs-12 col-sm-4"><div class="box-portfolio">Calculator</div></div>
+            </div>
+ 
+            <div class="row">
+                <div class="col-xs-12 col-sm-4"><div class="box-portfolio">4</div></div>
+                <div class="col-xs-12 col-sm-4"><div class="box-portfolio">5</div></div>
+                <div class="col-xs-12 col-sm-4"><div class="box-portfolio">6</div></div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-section-a -->
+ <!-- Banner -->
+    <div class="banner"><a  name="connections"></a>
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <h2>DANIEL<span class="lg-text">j</span>CARROLL</h2>
+                </div>
+                <nav class="col-xs-12 text-center">
+                    <a href="https://twitter.com/primalRambler" target="_blank"><i class="fa fa-twitter icon-social fa-fw"></i></a>
+                    <a href="https://github.com/primalrambler" target="_blank"><i class="fa fa-github icon-social"></i></a>
+                    <a href="http://linkedin.com/in/djcarroll-1993" target="_blank"><i class="fa fa-linkedin icon-social"></i></a>
+                </nav>
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <ul class="list-inline link-list">
+                        <li>
+                            <a href="/index.php">Home</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="/resume/">Resume</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="/portfolio">Portfolio</a>
+                        </li>
+                        <li class="footer-menu-divider">&sdot;</li>
+                        <li>
+                            <a href="/contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row bueller-padding"></div>
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.banner -->
+
+
+</html>
+</body>
